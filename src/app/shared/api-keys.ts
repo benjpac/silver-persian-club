6b372b27b7f19996185f6cbc224985da3d7d6d35
benjpac <benjpac@users.noreply.github.com>
2017-06-27 09:22:28 -0700

    Delete api-keys.ts

--- src/app/shared/api-keys.ts
+++
@@ -1,8 +0,0 @@
-export const masterFirebaseConfig = {
-  apiKey: 'AIzaSyAjrnaBqu3BozTSMJvfOWXXF9lnrcCsrBQ',
-  authDomain: 'silver-persian-club.firebaseapp.com',
-  databaseURL: 'https://silver-persian-club.firebaseio.com',
-  projectId: 'silver-persian-club',
-  storageBucket: 'silver-persian-club.appspot.com',
-  messagingSenderId: '103419295'
-}
